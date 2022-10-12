db7b2bacf433067f9c12ddc155a6086211524a0e
Chinmaya Panigrahy <65681668+Chinmaya-Panigrahy@users.noreply.github.com>
2022-10-12 19:47:50 +0530

    Add files via upload

---
+++ Script_Gray.m
@@ -0,0 +1,31 @@
+% If you use this code, then please cite our following paper:
+% Fractal dimension based parameter adaptive dual channel PCNN for multi-focus image fusion. 
+% Optics and Lasers in Engineering 133 (2020): 106141.
+
+clear all
+img1=imread('clock1.bmp');%First Source image
+img2=imread('clock2.bmp');   %Second source image
+tic
+img1= double(img1)/255;%Image Normalization
+img2= double(img2)/255;%Image Normalization
+Fc=FUSION_NSCT_ABS_MSMFM_DBC_PADCPCNN(img1,img2); 
+F=uint8(Fc*255);%Image Standardization
+toc
+figure, imshow(F)
+
+        
+      
+        
+        
+
+       
+
+   
+        
+
+        
+        
+   
+
+         
+
